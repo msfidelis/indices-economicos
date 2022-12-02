f671e05e16f62ac17cfd510b5d8ae20daba48f5e
Matheus Fidelis <matheus@nanoshots.com.br>
2022-12-02 13:15:16 -0300

    PIB

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"crawlers/pkg/logger"
-	"crawlers/runners/igpm"
-	"crawlers/runners/inpc"
-	"crawlers/runners/ipca"
-	"crawlers/runners/pib"
-	"crawlers/runners/selic"
+	// "crawlers/runners/igpm"
+	// "crawlers/runners/inpc"
+	// "crawlers/runners/ipca"
+	// "crawlers/runners/pib"
+	// "crawlers/runners/selic"
+	"crawlers/runners/gini"
 )
 
 func main() {
@@ -14,33 +15,40 @@ func main() {
 	l.Info().
 		Msg("Iniciando o processo de Crawling dos dados abertos")
 
-	// PIB
+	// // PIB
+	// l.Info().
+	// 	Msg("Iniciando o Runner de PIB")
+
+	// pib.Runner()
+
+	// // IPCA
+	// l.Info().
+	// 	Msg("Iniciando o Runner de IPCA")
+
+	// ipca.Runner()
+
+	// // INPC
+	// l.Info().
+	// 	Msg("Iniciando o Runner de INPC")
+
+	// inpc.Runner()
+
+	// // IGP-M
+	// l.Info().
+	// 	Msg("Iniciando o Runner de IGP-M")
+
+	// igpm.Runner()
+
+	// // SELIC
+	// l.Info().
+	// 	Msg("Iniciando o Runner de Selic")
+
+	// selic.Runner()
+
+	// GINI
 	l.Info().
-		Msg("Iniciando o Runner de PIB")
+		Msg("Iniciando o Runner de coeficiente de Gini")
 
-	pib.Runner()
-
-	// IPCA
-	l.Info().
-		Msg("Iniciando o Runner de IPCA")
-
-	ipca.Runner()
-
-	// INPC
-	l.Info().
-		Msg("Iniciando o Runner de INPC")
-
-	inpc.Runner()
-
-	// IGP-M
-	l.Info().
-		Msg("Iniciando o Runner de IGP-M")
-
-	igpm.Runner()
-
-	l.Info().
-		Msg("Iniciando o Runner de Selic")
-
-	selic.Runner()
+	gini.Runner()
 
 }
